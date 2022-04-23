60c87608cd6cd55e4fbffd386744642dce87a727
Elvis Arturo Tezen Villanueva <elvis.tezen@softtek.com>
2022-04-23 04:16:59 -0500

    tercero

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      {/* <Filter onChange={filterArts} /> */}
+      <Filter onChange={filterArts} />
       <Gallery arts={galleryData} />
       <Footer />
       <DarkMode />
